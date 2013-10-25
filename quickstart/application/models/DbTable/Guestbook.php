ad81cd85960899dc8b7a1385d3be3fe27207da94
Alejandro Cornejo <alejandro.cornejo@splendia.com>
2013-10-25 16:41:27 +0200

    get back mapper

--- quickstart/application/models/DbTable/Guestbook.php
+++ quickstart/application/models/DbTable/Guestbook.php
@@ -5,7 +5,13 @@ class Application_Model_DbTable_Guestbook extends Zend_Db_Table_Abstract
 
     protected $_name = 'guestbook';
 
-    protected $_dependentTables = array('Users');
+    protected $_referenceMap = array(
+        'Users' => array(
+            'columns' => 'user_id',
+            'refTableClass' => 'Application_Model_DbTable_Users',
+            'refColumns' => 'id'
+        )
+    );
 
 }
 
