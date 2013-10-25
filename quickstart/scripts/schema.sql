203832ab3b0baf6faa4379dcc74a11d2c8b87c55
Alejandro Cornejo <alejandro.cornejo@splendia.com>
2013-10-25 10:59:14 +0200

    users table

--- quickstart/scripts/schema.sql
+++ quickstart/scripts/schema.sql
@@ -5,3 +5,12 @@ CREATE TABLE IF NOT EXISTS guestbook (
     PRIMARY KEY (id),
     INDEX `date_index` USING BTREE (created)
 );
+
+CREATE TABLE IF NOT EXISTS users (
+    id          INT NOT NULL AUTO_INCREMENT,
+    username    VARCHAR(50),
+    password    VARCHAR(50),
+    name        VARCHAR(100) NULL,
+    address     VARCHAR(150) NULL,
+    PRIMARY KEY (id)
+);
