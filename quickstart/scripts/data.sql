ee6fe4a68897557ddb9d17a9a04c196034db9868
Alejandro Cornejo <alejandro.cornejo@splendia.com>
2013-10-25 11:05:52 +0200

    users data

--- quickstart/scripts/data.sql
+++ quickstart/scripts/data.sql
@@ -11,3 +11,8 @@ VALUES
     'Baz baz baz, baz baz Baz baz baz - baz baz baz.',
     NOW()
 );
+
+REPLACE INTO users
+    (username, password, name, address)
+VALUES
+    ('admin', SHA1('admin'), 'Administrator', '');
