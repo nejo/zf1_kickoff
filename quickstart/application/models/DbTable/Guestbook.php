936cc4d9fea867f886455b631a935aba6a418dc8
Alejandro Cornejo <acornejovila@gmail.com>
2013-10-25 18:44:52 +0200

    fixing the relationship

--- quickstart/application/models/DbTable/Guestbook.php
+++ quickstart/application/models/DbTable/Guestbook.php
@@ -9,9 +9,9 @@ class Application_Model_DbTable_Guestbook extends Zend_Db_Table_Abstract
 
     protected $_referenceMap = array(
         'User' => array(
-            'columns'       => 'user_id',   // the column in the 'guestbook' table which is used for the join
-            'refTableClass' => 'users',     // the users table name
-            'refColumns'    => 'id'         // the primary key of the users table
+            'columns'       => 'user_id',                           // the column in the 'guestbook' table which is used for the join
+            'refTableClass' => 'Application_Model_DbTable_Users',   // the users classname for users table
+            'refColumns'    => 'id'                                 // the primary key of the users table
         )
     );
 
--- quickstart/application/models/DbTable/Users.php
+++ quickstart/application/models/DbTable/Users.php
@@ -5,7 +5,7 @@ class Application_Model_DbTable_Users extends Zend_Db_Table_Abstract
 
     protected $_name = 'users';
 
-    protected $_rowClass = 'Model_Row_User';
+    protected $_rowClass = 'Application_Model_Row_User';
 
     protected $_dependentTables = array('Model_DbTable_Guestbook');
 
