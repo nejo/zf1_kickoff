69bda67a3ab69c0ed27c86b1c878cd982136fb2c
Alejandro Cornejo <alejandro.cornejo@splendia.com>
2013-10-28 17:59:05 +0100

    phpdoc

--- quickstart/library/Base/Model/Mapper/Base.php
+++ quickstart/library/Base/Model/Mapper/Base.php
@@ -2,8 +2,17 @@
 
 class Base_Model_Mapper_Base
 {
+    /**
+     * @var mixed|Zend_Db_Table_Abstract
+     */
     protected $_dbTable;
 
+    /**
+     * @param string|Zend_Db_Table_Abstract $dbTable
+     *
+     * @return $this
+     * @throws Exception
+     */
     public function setDbTable($dbTable)
     {
         if (is_string($dbTable)) {
@@ -17,6 +26,9 @@ class Base_Model_Mapper_Base
         return $this;
     }
 
+    /**
+     * @return mixed|Zend_Db_Table_Abstract
+     */
     public function getDbTable()
     {
         if (null === $this->_dbTable) {
@@ -26,6 +38,9 @@ class Base_Model_Mapper_Base
         return $this->_dbTable;
     }
 
+    /**
+     * @return Zend_Db_Table_Rowset_Abstract
+     */
     public function fetchAll()
     {
         return $this->getDbTable()->fetchAll();
