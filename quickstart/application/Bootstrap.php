7f0a792b74743605ff4b9d3bdbc71264791bdf09
Alejandro Cornejo <alejandro.cornejo@splendia.com>
2013-10-28 16:18:26 +0100

    caching ini file

--- quickstart/application/Bootstrap.php
+++ quickstart/application/Bootstrap.php
@@ -9,10 +9,19 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 
         $router = $this->getResource('FrontController')->getRouter();
 
-        $myRoutes = new Zend_Config_Ini(
-            APPLICATION_PATH . DIRECTORY_SEPARATOR . 'configs' . DIRECTORY_SEPARATOR . 'routes.ini',
-            APPLICATION_ENV
-        );
+        $cacheManager = $this->bootstrap('cachemanager')->getResource('cachemanager');
+        $cache = $cacheManager->getCache('cache');
+        $cacheId = 'routes';
+
+        $myRoutes = $cache->load($cacheId);
+
+        if (!$myRoutes) {
+            $myRoutes = new Zend_Config_Ini(
+                APPLICATION_PATH . DIRECTORY_SEPARATOR . 'configs' . DIRECTORY_SEPARATOR . 'routes.ini',
+                APPLICATION_ENV
+            );
+            $cache->save($myRoutes, $cacheId);
+        }
 
         $router->addConfig($myRoutes, 'routes');
     }
