bb7b2dfcf6c54d05290bd0449a1de862937de990
Alejandro Cornejo <alejandro.cornejo@splendia.com>
2013-10-28 17:48:49 +0100

    auto select the dbtable model

--- quickstart/library/Base/Model/Mapper/Base.php
+++ quickstart/library/Base/Model/Mapper/Base.php
@@ -17,4 +17,28 @@ class Base_Model_Mapper_Base
         return $this;
     }
 
+    public function getDbTable()
+    {
+        if (null === $this->_dbTable) {
+            $this->setDbTable($this->_getModelClassName());
+        }
+
+        return $this->_dbTable;
+    }
+
+    /**
+     * @return string
+     */
+    protected function _getModelClassName()
+    {
+        $separator = '_';
+        $modelClassName = 'Application_Model_DbTable';
+
+        $mapperClassName = get_class($this);
+        $lastSeparatorPosition = strrpos($mapperClassName, $separator);
+        $suffix = substr($mapperClassName, $lastSeparatorPosition);
+
+        return $modelClassName . $suffix;
+    }
+
 }
