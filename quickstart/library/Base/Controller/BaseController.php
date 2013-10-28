c95040c00e3b7192aab07883ffc08b9478f93e03
Alejandro Cornejo <alejandro.cornejo@splendia.com>
2013-10-28 16:34:08 +0100

    from bootstrap to base controller

--- quickstart/library/Base/Controller/BaseController.php
+++ quickstart/library/Base/Controller/BaseController.php
@@ -5,7 +5,39 @@ class Base_Controller_BaseController extends Zend_Controller_Action
 
     public function init()
     {
-        
+        $this->_initDoctype();
+        $this->_initHeadtitle();
+        $this->_initMeta();
+        $this->_initStylesheets();
+        $this->_initJavascripts();
+    }
+
+    protected function _initDoctype()
+    {
+        $this->view->doctype('HTML5');
+    }
+
+    protected function _initHeadtitle()
+    {
+        $this->view->headTitle('Zend Framework 1 Quickstart Application');
+        $this->view->headTitle()->setSeparator(' | ');
+    }
+
+    protected function _initMeta()
+    {
+        $this->view->headMeta()->setHttpEquiv('Content-Type', 'text/html; charset=utf-8');
+    }
+
+    protected function _initStylesheets()
+    {
+        $this->view->headLink()->appendStylesheet('/css/bootstrap.min.css');
+        $this->view->headLink()->appendStylesheet('/css/bootstrap-responsive.min.css');
+        $this->view->headLink()->appendStylesheet('/css/layout.css');
+    }
+
+    protected function _initJavascripts()
+    {
+        $this->view->headScript()->appendFile('/js/bootstrap.min.js');
     }
 
 }
