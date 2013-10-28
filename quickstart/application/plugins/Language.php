0c1481ebc88fde9a49dfe3a98b6296392eb3eadc
Alejandro Cornejo <alejandro.cornejo@splendia.com>
2013-10-28 12:45:10 +0100

    improving language selection

--- quickstart/application/plugins/Language.php
+++ quickstart/application/plugins/Language.php
@@ -4,7 +4,6 @@ class Application_Plugin_Language
     extends Zend_Controller_Plugin_Abstract
 {
     /**
-     * @todo check requested to the saved in preferences
      * @link http://framework.zend.com/manual/1.12/en/zend.locale.functions.html
      *
      * @param Zend_Controller_Request_Abstract $request
@@ -13,21 +12,19 @@ class Application_Plugin_Language
     {
         $lang = $request->getParam('lang', null);
 
-        if (!empty($lang)) {
-            $translate = Zend_Registry::get('Zend_Translate');
+        $translate = Zend_Registry::get('Zend_Translate');
 
-            $locale = new Zend_Locale($lang);
+        $locale = new Zend_Locale($lang);
 
-            if ($translate->isAvailable($locale)) {
-                $translate->setLocale($locale);
-            }
-
-            // Set language to global param so that our language route can
-            // fetch it nicely.
-            $front = Zend_Controller_Front::getInstance();
-            $router = $front->getRouter();
-            $router->setGlobalParam('lang', $lang);
+        if ($translate->isAvailable($locale)) {
+            $translate->setLocale($locale);
         }
+
+        // Set language to global param so that our language route can
+        // fetch it nicely.
+        $front = Zend_Controller_Front::getInstance();
+        $router = $front->getRouter();
+        $router->setGlobalParam('lang', $lang);
 
     }
 }
