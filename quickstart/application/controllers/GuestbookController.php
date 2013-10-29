1df2189a7529d2479902a55fc559b9571ae55de9
Alejandro Cornejo <alejandro.cornejo@splendia.com>
2013-10-29 10:20:06 +0100

    phpmd

--- quickstart/application/controllers/GuestbookController.php
+++ quickstart/application/controllers/GuestbookController.php
@@ -11,15 +11,14 @@ class GuestbookController extends Base_Controller_BaseController
 
     public function signAction()
     {
-        $request = $this->getRequest();
-        $form    = new Application_Form_Guestbook();
+        $form = new Application_Form_Guestbook();
 
         $usersMapper = new Application_Model_Mapper_Users();
         $usersSelector = $usersMapper->getUsersSelector();
         $form->getElement('userId')->setMultiOptions($usersSelector);
 
         if ($this->getRequest()->isPost()) {
-            if ($form->isValid($request->getPost())) {
+            if ($form->isValid($this->getRequest()->getPost())) {
                 $comment = new Application_Model_Guestbook($form->getValues());
                 $mapper  = new Application_Model_GuestbookMapper();
                 $mapper->save($comment);
