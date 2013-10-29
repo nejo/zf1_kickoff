37f082d4912631a5f8f179d5332a30f2866ca882
Alejandro Cornejo <alejandro.cornejo@splendia.com>
2013-10-29 10:24:15 +0100

    filter form

--- quickstart/application/controllers/VideosController.php
+++ quickstart/application/controllers/VideosController.php
@@ -5,7 +5,13 @@ class VideosController extends Base_Controller_BaseController
 
     public function indexAction()
     {
-        // action body
+        $filterForm = new Application_Form_Filter_User();
+
+        $usersMapper = new Application_Model_Mapper_Users();
+        $usersSelector = $usersMapper->getUsersSelector();
+        $filterForm->getElement('userId')->setMultiOptions($usersSelector);
+
+        $this->view->filterForm = $filterForm;
     }
 
 
