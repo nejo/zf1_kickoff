61c4b02a2ce65b749fa59d705ba40c98d67b0b0d
Alejandro Cornejo <acornejovila@gmail.com>
2013-10-29 08:25:33 +0100

    data added

--- quickstart/scripts/data.sql
+++ quickstart/scripts/data.sql
@@ -23,4 +23,34 @@ REPLACE INTO users
         'administrator',
         NOW(),
         'Administrator Name'
+    ),
+    (
+        2,
+        'user',
+        SHA1('userj56jkb46j7b56jb475h8b4578bjk5b8ljk45hkhkg'),
+        'j56jkb46j7b56jb475h8b4578bjk5b8ljk45hkhkg',
+        'user',
+        NOW(),
+        'User Name'
     );
+
+REPLACE INTO channels
+    (id, name, user_id)
+VALUES
+    (1, 'nature', 1),
+    (2, 'sports', 1),
+    (3, 'sports', 2),
+    (4, 'music', 2),
+    (5, 'dance', 2);
+
+REPLACE INTO videos
+    (id, title, youtube_key, user_id)
+VALUES
+    (1, 'niagara falls', 'yuyu', 1),
+    (2, 'rainbow', 'yuyu link', 1),
+    (3, 'ac dc 2012 tour', 'yuyu', 2);
+
+REPLACE INTO channels_videos
+    (channel_id, video_id)
+VALUES
+    (1, 1), (1, 2), (4, 3), (5, 3);
