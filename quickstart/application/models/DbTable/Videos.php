b87c94c7f5f6255242197f657300cb4b8a7a409d
Alejandro Cornejo <alejandro.cornejo@splendia.com>
2013-10-29 10:37:40 +0100

    setting relation with users

--- quickstart/application/models/DbTable/Videos.php
+++ quickstart/application/models/DbTable/Videos.php
@@ -5,6 +5,12 @@ class Application_Model_DbTable_Videos extends Zend_Db_Table_Abstract
 
     protected $_name = 'videos';
 
-
+    protected $_referenceMap = array(
+        'User' => array(
+            'columns'       => 'user_id',                           // the column in the 'videos' table which is used for the join
+            'refTableClass' => 'Application_Model_DbTable_Users',   // the users classname for users table
+            'refColumns'    => 'id'                                 // the primary key of the users table
+        )
+    );
 }
 
